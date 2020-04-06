9132d1d327f714319a008ef9b4b566a2e400f05e
TGAAM <bassartemix@gmail.com>
2020-04-05 20:16:16 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    if message.content.startswith('%beep'):
+        await message.channel.send('Boop!')
+'''
     if message.content.startswith('%hi'):
         await message.channel.send('Hello!')
 
@@ -21,5 +23,6 @@ async def on_message(message):
 
     if message.content.startswith('%giveaway') and message.channel.name == 'giveaway-announcements':
         await client.get_channel(694645573625708565).send('msg: ' + message.content)
+'''
 
 client.run(os.environ['TOKEN'])
