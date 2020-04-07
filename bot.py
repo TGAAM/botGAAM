457b0661f4a009088402f5a0b61984325a06408c
TGAAM <bassartemix@gmail.com>
2020-04-07 08:27:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,47 +8,90 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 client = discord.Client()
-cursor = conn.cursor()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
     #setup the table if it's not already
-    sqlTable = "create table if not exists users(user_id bigint, hi_count int);"
+    cursor = conn.cursor()
+
+    # uncomment to reset the table on next/each load
+    #cursor.execute("drop table users")
+    #conn.commit()
+    
+    sqlTable = "create table if not exists users(user_id bigint not null, hi_count int, check_count int, primary key(user_id));"
     cursor.execute(sqlTable)
     conn.commit()
+
+    print('We have logged in as {0.user}'.format(client))
     
 
 @client.event
 async def on_message(message):
+    cursor = conn.cursor()
+
     if message.author == client.user:
         return
     if message.content.startswith('%beep'):
         await message.channel.send('Boop!')
         
     if message.content.startswith('$s'):
-        await message.channel.send('I predict a shiny in ' + str(random.randint(1, 10000)) + ' frames')
+        cursor.execute("select count(*) from users where user_id = " + str(message.author.id))
+        userExists = cursor.fetchone()[0]
 
-    if (message.channel == client.get_channel(696864617653076078)):
-        if message.content.startswith('%g start'):
-            await message.channel.send('Creating giveaway')
-
-    if (message.content.startswith('$hi')):
-        cursor.execute("select hi_count from users where user_id = " + str(message.author.id))
-        hiCount = 1 + cursor.fetchone()[0]
-
-        if (hiCount == 1):
-            cursor.execute('insert into users(user_id, hi_count) values ('+str(message.author.id)+', 1)')
+        if (userExists == 0):
+            cursor.execute("insert into users(user_id, check_count) values ("+str(message.author.id)+", 1)")
+            conn.commit()
+        else:
+            cursor.execute("select check_count from users where user_id = " + str(message.author.id))
+            checkCount = 1 + cursor.fetchone()[0]
+            cursor.execute("update users set check_count = " + str(checkCount) + "where user_id = " + str(message.author.id))
             conn.commit()
 
-        await message.channel.send("You've said hi to MuMu " + str(hiCount) + " times since I started counting" )
+        await message.channel.send('I predict a shiny in ' + str(random.randint(1, 10000)) + ' frames')
 
-        cursor.execute('update users set hi_count = ' + str(hiCount) + 'where user_id = ' + str(message.author.id))
-        conn.commit()
+
+    if (message.content.startswith('$hi')):
+        cursor.execute("select count(*) from users where user_id = " + str(message.author.id))
+        userExists = cursor.fetchone()[0]
+
+        if (userExists == 0):
+            cursor.execute("insert into users(user_id, hi_count) values ("+str(message.author.id)+", 1)")
+            conn.commit()
+        else:
+            cursor.execute("select hi_count from users where user_id = " + str(message.author.id))
+            hiCount = 1 + cursor.fetchone()[0]
+            cursor.execute("update users set hi_count = " + str(hiCount) + "where user_id = " + str(message.author.id))
+            conn.commit()
+
 
     if (message.content.startswith('$dynamax')):
             await message.channel.send("🙀 🙀 Are you really <:gmax_gengar:696490246057099304> Dynamaxing <:gmax_gengar:696490246057099304>?!?!?! 🤔 🤔 B-But have u checked the 📌 📌 pins?!!❗ I'm not 🙅 🙅‍♀️ allowing it!!! 🙏 🙏 Please, don't Dynamax ⛔ ⛔")
+
+
+    if (message.content.startswith('%fetch')):
+        msgResults = ''
+
+        inputStr = message.content.split(' ')
+
+        if (len(inputStr) > 2):
+            await message.channel.send('Too many input parameters')
+            return
+
+        cursor.execute("select count(*) from users where user_id = " + inputStr[1])
+        userExists = cursor.fetchone()[0]
+        
+
+        if (userExists == 0):
+            msgResults = 'User not found'
+        else:
+            cursor.execute("select * from users where user_id = " + inputStr[1])
+            msgResults = str(cursor.fetchone())
+
+        await message.channel.send(msgResults)
+
+
+
+
 
 '''
     if message.content.startswith('%hi'):
