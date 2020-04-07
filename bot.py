f77d29438d5ce06755bcbdcd6f479b9ec34759a5
TGAAM <bassartemix@gmail.com>
2020-04-06 20:38:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import os
 import random
 import psycopg2
 
+DATABASE_URL = os.environ['DATABASE_URL]
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+client = discord.Client()
 
 @client.event
 async def on_ready():
