d96d9091295b67b06513e157db94f6da6e625573
TGAAM <bassartemix@gmail.com>
2020-04-06 20:28:34 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import discord
 import os
 import random
+import psycopg2
 
-
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
 
 @client.event
 async def on_message(message):
@@ -18,6 +21,11 @@ async def on_message(message):
         
     if message.content.startswith('$s'):
         await message.channel.send('I predict a shiny in ' + str(random.randint(1, 10000)) + ' frames')
+
+    if (message.channel = client.get_channel(696864617653076078)):
+        if message.content.startswith('%g start'):
+            await message.channel.send('Creating giveaway')
+    
 '''
     if message.content.startswith('%hi'):
         await message.channel.send('Hello!')
@@ -26,5 +34,7 @@ async def on_message(message):
     if message.content.startswith('%giveaway') and message.channel.name == 'giveaway-announcements':
         await client.get_channel(694645573625708565).send('msg: ' + message.content)
 '''
+
+
 
 client.run(os.environ['TOKEN'])
