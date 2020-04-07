47fd41c4c742d68ad5baacbe77d7aa88ba5bd5ab
TGAAM <bassartemix@gmail.com>
2020-04-06 21:18:07 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,20 @@ import os
 import random
 import psycopg2
 
+
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 client = discord.Client()
+cursor = conn.cursor()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+    #setup the table if it's not already
+    sqlTable = "create table if not exists users(user_id bigint, hi_count int);"
+    cursor.execute(sqlTable)
+    conn.commit()
     
 
 @client.event
@@ -25,7 +32,18 @@ async def on_message(message):
     if (message.channel == client.get_channel(696864617653076078)):
         if message.content.startswith('%g start'):
             await message.channel.send('Creating giveaway')
-    
+
+    if (message.content.startswith('$hi')):
+        hiCount = 1 + int(cursor.execute("select hi_count from users where user_id = " + str(message.author.id)) or 0)
+        if (hiCount == 1):
+            cursor.execute("insert into users(user_id, hi_count) values ("+str(message.author.id)+", 1)")
+            conn.commit()
+
+        await message.channel.send("You've said hi to MuMu " + str(hiCount) + " times since I started counting" )
+
+        cursor.execute('update users set hi_count = ' + str(hiCount) + "where user_id = " + str(message.author.id))
+        conn.commit()
+
 '''
     if message.content.startswith('%hi'):
         await message.channel.send('Hello!')
@@ -34,7 +52,6 @@ async def on_message(message):
     if message.content.startswith('%giveaway') and message.channel.name == 'giveaway-announcements':
         await client.get_channel(694645573625708565).send('msg: ' + message.content)
 '''
-
 
 
 client.run(os.environ['TOKEN'])
