bab0460eee596c84f37a6987e68035ea94d38c1a
TGAAM <bassartemix@gmail.com>
2020-05-15 10:41:31 -0400

    Error handling added

--- botgaamTestFunctions.py
+++ botgaamTestFunctions.py
@@ -1,10 +1,3 @@
-# import modules
-# For hosting from Heroku/Github I had to include a Requirements.txt file with the following
-'''
-discord.py==1.3.3
-psycopg2==2.8.5
-'''
-
 import discord
 import os
 import random
@@ -25,6 +18,7 @@ whitelistChannelsDev = [697060204571000903, 698135978270916678, 6981359872258867
 
 # setup a discord client
 bot = commands.Bot(command_prefix="!", case_insensitive=True, description="Coding testing Bot")
+
 
 
 # don't work in DMs
@@ -49,43 +43,16 @@ async def is_dev(ctx):
 # initial setup 
 @bot.event
 async def on_ready():
-    cogs = ['cogs.mumu_plus']
-    for cog in cogs:
-        try:
-            bot.load_extension(cog)
-        except Exception as e:
-            print("Problem with loading: " + str(cog))
-            traceback.print_exc()
-
     # notify the log that it's ready
     print('We have logged in as {0.user}'.format(bot))
 
 
-@bot.command()
-@commands.check(is_dev_room)
-async def beep(ctx):
-    await ctx.send("Boop!")
-    return
-
-@bot.command()
-async def dynamax(ctx):
-    await ctx.send("🙀 🙀 Are you really <:gmax_gengar:696490246057099304> Dynamaxing <:gmax_gengar:696490246057099304>?!?!?! 🤔 🤔 B-But have u checked the 📌 📌 pins?!!❗ I'm not 🙅 🙅‍♀️ allowing it!!! 🙏 🙏 Please, don't Dynamax ⛔ ⛔")
-    return
 
 
-# error handling
-@bot.event
-async def on_command_error(ctx, error):
-    # ignore errors that are invalid commands or calls from failed checks for now
-    if isinstance(error, CommandNotFound):
-        return
-    elif isinstance(error, CheckFailure):
-        return
-    elif isinstance(error, MissingRequiredArgument):
-        await ctx.send("You need to include more details for this command")
-        return
 
-    raise error
+
+
+
 
 # start the program
 # I had to manually add the bot token to Heroku's config variables
