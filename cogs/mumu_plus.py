3dd3b266911987d86ccfeda02d40f0fd71266ba8
TGAAM <bassartemix@gmail.com>
2020-06-17 18:52:19 -0400

    Temp MuMu commands

--- cogs/mumu_plus.py
+++ cogs/mumu_plus.py
@@ -17,9 +17,22 @@ class MuMu_plus (commands.Cog):
             # send a guess on how far away the shiny is. Playing around with random numbers
             # if it somehow guesses right people are going to freak out
             #await message.channel.send('I predict a shiny in ' + str(random.randint(1, 10000)) + ' frames')
-            await message.channel.send("Remember that the best seeds are reserved for the best hosts, those of Team Teal <:wigglyteal:720089522766872606>")
-
+            #await message.channel.send("Remember that the best seeds are reserved for the best hosts, those of Team Teal <:wigglyteal:720089522766872606>")
+            await message.channel.send("<:catcry:722945917740908584>")
             return
+
+        if message.content.startswith('$ping'):
+            await message.channel.send("Pong!")
+            return
+
+        if message.content.startswith('$pong'):
+            await message.channel.send("Ping!")
+            return
+
+        if message.content.startswith('$hi'):
+            await message.channel.send("I'm no MuMu but hi anyway!")
+            return
+
 
         return
             
